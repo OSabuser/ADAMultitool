409e735682351cdbd36de49376f5724c39c3f7dd
Dmitry Akimov <von.akimov@gmail.com>
2025-06-18 13:04:19 +0300

    # try handshake

--- protocol/src/host.rs
+++ protocol/src/host.rs
@@ -1,3 +1,4 @@
+use crate::error;
 use crate::{error::DeviceConnectError, error::ProtoRecvError, error::ProtoSendError};
 use crate::{error::FrameDecodeError, mu_frame::MUFrame};
 use std::time::Duration;
@@ -11,24 +12,35 @@ impl HostClient {
         port_name: &str,
         baudrate: u32,
         timeout: Duration,
-    ) -> Result<HostClient, serialport::Error> {
+    ) -> Result<HostClient, error::DeviceConnectError> {
         let serial_port = serialport::new(port_name, baudrate)
             .timeout(timeout)
-            .open()?;
+            .open()
+            .expect(format!("Unable to open: {}", port_name).as_str());
 
-        Ok(HostClient { serial_port })
+        Self::try_handshake(serial_port)
     }
 
     fn try_handshake(
         mut instance: Box<dyn serialport::SerialPort + 'static>,
-    ) -> Result<Self, serialport::Error> {
+    ) -> Result<Self, error::DeviceConnectError> {
         let mut frame = MUFrame::new();
-        frame.set_data(b"hello\n".to_vec()).unwrap();
-        crate::send_proto_message(frame, &mut instance).unwrap();
+
+        frame
+            .set_data(b"hello\n".to_vec())
+            .map_err(|_| DeviceConnectError::BadHandshake.into())?;
+
+        crate::send_proto_message(frame, &mut instance)
+            .map_err(|_| DeviceConnectError::BadHandshake.into())?;
 
         let buf = Vec::new();
 
-        let answer = crate::recv_proto_message(&buf[..]).unwrap();
+        let answer = crate::recv_proto_message(&buf[..])
+            .map_err(|_| DeviceConnectError::BadHandshake.into())?;
+
+        if answer.get_data() != b"Hi!\r\n" {
+            return Err(DeviceConnectError::BadHandshake.into());
+        }
 
         Ok(Self {
             serial_port: instance,
